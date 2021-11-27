1861c43a4df0ccf2ff4b8d67f7ff0058f28db994
Anthony Rentsch <anthony.rentsch@gmail.com>
2021-11-27 10:13:11 -0500

    add player win rate table

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # set up
 import dash
-import dash_table
+from dash import dash_table
 from dash import dcc
 from dash import html 
 from dash.dependencies import Input, Output 
@@ -22,7 +22,7 @@ app.layout = html.Div(
             'Terraforming Mars Statistics',
             style = {'text-align' : 'center'}
         ),
-        dcc.Tabs(id="app-tabs", value='tab-1-example-graph', children=[
+        dcc.Tabs(id="app-tabs", value='app-tabs', children=[
             dcc.Tab(label='Summary', value='summary-tab'),
             dcc.Tab(label='Individual Player Statistics', value='individual-player-tab'),
             dcc.Tab(label='Player ELO', value='player-elo-tab'),
@@ -35,21 +35,25 @@ app.layout = html.Div(
 @app.callback(Output('tab-content', 'children'),
               Input('app-tabs', 'value'))
 def render_content(tab):
-    if tab == 'Summary':
+    if tab == 'summary-tab':
+        player_win_rates_df = get_player_win_rates_table(df)
         return html.Div([
             html.H3('Player Win Rates'),
-            dcc.Graph(
-                id='graph-1-tabs',
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [3, 1, 2],
-                        'type': 'bar'
-                    }]
-                }
+            dash_table.DataTable(
+                id='player-win-rates-table',
+                columns=[{"name": i, "id": i} for i in player_win_rates_df.columns],
+                data=player_win_rates_df.to_dict('records'),
             )
         ])
-    return 
+    else:
+        return html.Div([
+            html.H3('Coming soon...')
+        ])
+
+def get_player_win_rates_table(df):
+    return df.groupby('player').\
+    agg(wins = ('is_winner','sum'), games = ('is_winner', 'count'), win_rate = ('is_winner','mean')).\
+    sort_values(by='win_rate', ascending=False).reset_index()
 
 if __name__ == '__main__': 
     app.run_server(port=8000, host='127.0.0.1', debug=True)
